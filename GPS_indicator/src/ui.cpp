3b412b090c25f206c40169b5bb6c8fb46c143906
dnovichman <dnovichman@hotmail.com>
2016-12-09 12:22:24 -0500

    fixed GPS Indicator

--- GPS_indicator/src/ui.cpp
+++ GPS_indicator/src/ui.cpp
@@ -1,31 +1,3 @@
-/*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-// %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Float64MultiArray.h"
@@ -37,8 +9,6 @@
 #include <GL/glut.h>
 #include <pthread.h>
 
-#define DATA_LENGTH 1364 //138 FOR 1/10;		228 FOR 1/6;		114 FOR 1/12;	 172 FOR 1/8;	682 FOR 1/2;	1364 FOR 1;
-#define POINTS	682 // DATA_LENGTH/2
 #define PI 3.1415926
 
 using namespace std;
@@ -48,6 +18,7 @@ char *window_title = "Filter Indicator";
 int vicon = 0;
 int gps_sat = 0;
 int bff_filter = 0;
+float mag_vel = 0.0f;
 
 typedef enum {
    MODE_BITMAP,
@@ -290,9 +261,12 @@ void drawScene() {
 	    glRasterPos3f(173, 41,0);
 	    print_bitmap_string(bitmap_fonts[3], "^");
 	    glRasterPos3f(170, 30,0);
-	    print_bitmap_string(bitmap_fonts[3], "V =");
-	    glRasterPos3f(215, 30,0);
-	    print_bitmap_string(bitmap_fonts[3], "0");
+	    print_bitmap_string(bitmap_fonts[3], "|V| =");
+	    glRasterPos3f(215, 30,0); 
+	    char output[50];
+	    snprintf(output, 50, " %2.1f", mag_vel);
+
+	    print_bitmap_string(bitmap_fonts[3], output);
 	} 
 
 	glBegin(GL_POLYGON);
@@ -352,11 +326,6 @@ void* test(void* threadarg) {
         return 0;
 }
 
-
-
-
-
-// %Tag(CALLBACK)%
 void GPSCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 {
 	vicon = (int)msg->data[0];//0; 1
@@ -369,21 +338,17 @@ void GPSCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 	}
 	printf("vicon: %d, GPS_sat: %d, bff_filter: %d;\n", vicon, gps_sat, bff_filter);	
 }
-// %EndTag(CALLBACK)%
+
+void VehicleStateCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
+{
+	float vx = msg->data[6];
+	float vy = msg->data[7];
+	float vz = msg->data[8];
+	mag_vel = sqrt(vx*vx + vy*vy + vz*vz);
+}
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line. For programmatic
-   * remappings you can use a different version of init() which takes remappings
-   * directly, but for most command-line programs, passing argc and argv is the easiest
-   * way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-	
 	th_arg.th_argc=argc;
 	th_arg.th_argv=argv;
 	pthread_t thread;
@@ -391,44 +356,14 @@ int main(int argc, char **argv)
    int rc=pthread_create(&thread, NULL, test, (void*) &th_arg);  
 
   ros::init(argc, argv, "GPS_indicator");
-my_init();
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
+  my_init();
+
   ros::NodeHandle n;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
-  ros::Subscriber sub = n.subscribe("vicon_gps_state", 1, GPSCallback); //laser: raw data    scan: pose corrected data
-// %EndTag(SUBSCRIBER)%
+  ros::Subscriber sub = n.subscribe("vicon_gps_state", 1, GPSCallback); 
+  ros::Subscriber state_sub = n.subscribe("vehicle_state",1,VehicleStateCallback);
 
-
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-// %Tag(SPIN)%
   ros::spin();
-// %EndTag(SPIN)%
-
  
         return 0;
 }
-// %EndTag(FULLTEXT)%
